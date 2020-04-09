310a39a632efca0005abd487c5d4bf2ea3a5dce4
Chin Kuay <kuaychin@gmail.com>
2020-04-09 14:15:58 -0500

    updated

--- server/index.js
+++ server/index.js
@@ -2,18 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
 const massive = require("massive");
-
-const PORT = 4000;
-
-const { SESSION_SECRET, CONNECTION_STRING } = process.env;
-
+const authCtrl = require("./controllers/authController");
 const app = express();
+const { CONNECTION_STRING, SERVER_PORT, SESSION_SECRET } = process.env;
 
 app.use(express.json());
 
 massive(CONNECTION_STRING).then((db) => {
   app.set("db", db);
-  console.log("db connected");
 });
 
 app.use(
@@ -24,4 +20,10 @@ app.use(
   })
 );
 
-app.listen(PORT, () => console.log(`Big Bro listening ${PORT}`));
+app.post("/auth/register", authCtrl.register);
+app.post("/auth/login", authCtrl.login);
+app.get("/auth/logout", authCtrl.logout);
+
+app.listen(SERVER_PORT, () => {
+  console.log(`listening on port${SERVER_PORT}`);
+});
