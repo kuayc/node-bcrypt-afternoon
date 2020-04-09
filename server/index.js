c7318f3c15af288762bf94fae206d3e4501ff362
Chin Kuay <kuaychin@gmail.com>
2020-04-09 15:31:56 -0500

    updated

--- server/index.js
+++ server/index.js
@@ -3,28 +3,37 @@ const express = require("express");
 const session = require("express-session");
 const massive = require("massive");
 const authCtrl = require("./controllers/authController");
+const treasureCtrl = require("./controllers/treasureController");
+const auth = require("./middleware/authMiddleware");
+
+const { CONNECTION_STRING, SESSION_SECRET } = process.env;
 
 const app = express();
 
-const { CONNECTION_STRING, SERVER_PORT, SESSION_SECRET } = process.env;
-app.use(express.json());
+const PORT = 4000;
 
-massive(CONNECTION_STRING).then((db) => {
-  app.set("db", db);
-});
+app.use(express.json());
 
 app.use(
   session({
+    secret: SESSION_SECRET,
     resave: true,
     saveUninitialized: false,
-    secret: SESSION_SECRET,
   })
 );
 
 app.post("/auth/register", authCtrl.register);
 app.post("/auth/login", authCtrl.login);
 app.get("/auth/logout", authCtrl.logout);
+app.get("/api/treasure/dragon", treasureCtrl.dragonTreasure);
+app.get("/api/treasure/user", treasureCtrl.getUserTreasure);
+app.post("/api/treasure/user", auth.usersOnly, treasureCtrl.addUserTreasure);
+app.get("/api/treasure/all", auth.usersOnly, treasureCtrl.getAllTreasure);
 
-app.listen(SERVER_PORT, () => {
-  console.log(`listening on port${SERVER_PORT}`);
+massive(CONNECTION_STRING).then((db) => {
+  app.set("db", db);
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
 });
